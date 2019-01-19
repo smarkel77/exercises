4338073d4ae31947b2a64a7ed1e1a2154dec73f9
Steven Markel <smarkel@gmail.com>
2019-01-18 19:30:45 -0500

    3 out of 4 done

--- 05-Command_Line_Programs/src/main/java/com/techelevator/DecimalToBinary.java
+++ 05-Command_Line_Programs/src/main/java/com/techelevator/DecimalToBinary.java
@@ -1,9 +1,39 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class DecimalToBinary {
 
 	public static void main(String[] args) {
+		
+		/*
+		 * 
+		 * Write a command line program which prompts the user for a 
+		 * series of decimal integer values separated by spaces. Each 
+		 * decimal integer is displayed along with its equivalent binary value.
+		 * Please enter in a series of decimal values (separated by spaces): 460 8218 1 31313 987654321
 
+		460 in binary is 111001100
+		8218 in binary is 10000000011010
+		1 in binary is 1
+		31313 in binary is 111101001010001
+		987654321 in binary is 111010110111100110100010110001
+		 */
+		
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please enter in a series of decimal values (separated by spaces):");
+		String decimalInput = input.nextLine();
+		String[] decimalAsString = decimalInput.split(" ");
+		int[] newArray = new int [decimalAsString.length];
+		
+		for(int i = 0; i < decimalAsString.length; i++) {
+			newArray[i] = Integer.parseInt(decimalAsString[i]);
+			System.out.println(newArray[i] + " in binary is " + (int)(newArray[i]));
+		
+
+	}
 	}
 
 }
--- 05-Command_Line_Programs/src/main/java/com/techelevator/Fibonacci.java
+++ 05-Command_Line_Programs/src/main/java/com/techelevator/Fibonacci.java
@@ -1,9 +1,55 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class Fibonacci {
 
 	public static void main(String[] args) {
+		
+		/*
+		 The Fibonacci numbers are the integers in the following sequence:
 
+		0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ...
+		By definition, the first two numbers in the Fibonacci sequence are 0 and 1, and each subsequent number is the sum of the previous two.
+
+		Write a command line program which prompts the user for an integer value and display the Fibonacci sequence leading up to that number.
+
+		Please enter the Fibonacci number: 25
+
+		0, 1, 1, 2, 3, 5, 8, 13, 21
+		 */
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please enter the Fibonacci number:");
+		
+		String fibEnd = input.nextLine();
+		
+		int intFibEnd = Integer.parseInt(fibEnd);
+		
+		int a = 0;
+		int b = 1;
+		int c = 0;
+		System.out.print(a + ", " + b);
+		
+		while (c <= intFibEnd) {
+			if (c == 0) {
+				c = a + b;
+				a = b;
+				b = c;
+			}
+			
+			if (c != 0) {
+			System.out.print(", " + c);
+			
+			c = a + b;
+			a = b;
+			b = c;
+			
+		}
+		}
+		
+		input.close();
 	}
 
 }
