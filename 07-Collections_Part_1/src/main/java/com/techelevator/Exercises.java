b73c6a1f3c921ebff7c3256bfa172726d1e02cbf
Steven Markel <smarkel@gmail.com>
2019-01-23 13:26:12 -0500

    finished 07

--- 07-Collections_Part_1/src/main/java/com/techelevator/Exercises.java
+++ 07-Collections_Part_1/src/main/java/com/techelevator/Exercises.java
@@ -12,132 +12,246 @@ import java.util.Stack;
 public class Exercises {
 
 	/*
-	 Note, for-each is preferred, and should be used when possible.
+	 * Note, for-each is preferred, and should be used when possible.
 	 */
 
 	/*
-	 Given an array of Strings, return an ArrayList containing the same Strings in the same order 
-	 array2List( {"Apple", "Orange", "Banana"} )  ->  ["Apple", "Orange", "Banana"]
-	 array2List( {"Red", "Orange", "Yellow"} )  ->  ["Red", "Orange", "Yellow"]
-	 array2List( {"Left", "Right", "Forward", "Back"} )  ->  ["Left", "Right", "Forward", "Back"] 
+	 * Given an array of Strings, return an ArrayList containing the same Strings in
+	 * the same order array2List( {"Apple", "Orange", "Banana"} ) -> ["Apple",
+	 * "Orange", "Banana"] array2List( {"Red", "Orange", "Yellow"} ) -> ["Red",
+	 * "Orange", "Yellow"] array2List( {"Left", "Right", "Forward", "Back"} ) ->
+	 * ["Left", "Right", "Forward", "Back"]
 	 */
 	public List<String> array2List(String[] stringArray) {
-		return null;
+		List<String> newArray = new ArrayList<String>();
+		for (String stringArrayi : stringArray) {
+			newArray.add(stringArrayi);
+		}
+		return newArray;
 	}
-	
+
 	/*
-	 Given a list of Strings, return an array containing the same Strings in the same order 
-	 list2Array( ["Apple", "Orange", "Banana"] )  ->  {"Apple", "Orange", "Banana"}
-	 list2Array( ["Red", "Orange", "Yellow"] )  ->  {"Red", "Orange", "Yellow"}
-	 list2Array( ["Left", "Right", "Forward", "Back"] )  ->  {"Left", "Right", "Forward", "Back"}
+	 * Given a list of Strings, return an array containing the same Strings in the
+	 * same order list2Array( ["Apple", "Orange", "Banana"] ) -> {"Apple", "Orange",
+	 * "Banana"} list2Array( ["Red", "Orange", "Yellow"] ) -> {"Red", "Orange",
+	 * "Yellow"} list2Array( ["Left", "Right", "Forward", "Back"] ) -> {"Left",
+	 * "Right", "Forward", "Back"}
 	 */
 	public String[] list2Array(List<String> stringList) {
-		return null;
+		int counter = 0;
+		String[] newArray = new String[stringList.size()];
+		for (String stringListI : stringList) {
+			newArray[counter] = stringListI;
+			counter++;
+		}
+		return newArray;
 	}
-	
+
 	/*
-	 Given an array of Strings, return an ArrayList containing the same Strings in the same order 
-	 except for any words that contain exactly 4 characters.
-	 no4LetterWords( {"Train", "Boat", "Car"} )  ->  ["Train", "Car"]
-	 no4LetterWords( {"Red", "White", "Blue"} )  ->  ["Red", "White"]
-	 no4LetterWords( {"Jack", "Jill", "Jane", "John", "Jim"} )  ->  ["Jim"]
+	 * Given an array of Strings, return an ArrayList containing the same Strings in
+	 * the same order except for any words that contain exactly 4 characters.
+	 * no4LetterWords( {"Train", "Boat", "Car"} ) -> ["Train", "Car"]
+	 * no4LetterWords( {"Red", "White", "Blue"} ) -> ["Red", "White"]
+	 * no4LetterWords( {"Jack", "Jill", "Jane", "John", "Jim"} ) -> ["Jim"]
 	 */
 	public List<String> no4LetterWords(String[] stringArray) {
-		return null;
+		List<String> not4 = new ArrayList<String>();
+		for (String stringArrayI : stringArray) {
+			if (stringArrayI.length() != 4) {
+				not4.add(stringArrayI);
+			}
+		}
+		return not4;
 	}
 
-
 	/*
-	 Given a List of Strings, return a new list in reverse order of the original. One obvious solution is to
-	 simply loop through the original list in reverse order, but see if you can come up with an alternative
-	 solution. (Hint: Think LIFO (i.e. stack))
-	 reverseList( ["purple", "green", "blue", "yellow", "green" ])  -> ["green", "yellow", "blue", "green", "purple" ]
-	 reverseList( ["jingle", "bells", "jingle", "bells", "jingle", "all", "the", "way"} )
-		-> ["way", "the", "all", "jingle", "bells", "jingle", "bells", "jingle"]
+	 * Given a List of Strings, return a new list in reverse order of the original.
+	 * One obvious solution is to simply loop through the original list in reverse
+	 * order, but see if you can come up with an alternative solution. (Hint: Think
+	 * LIFO (i.e. stack)) reverseList( ["purple", "green", "blue", "yellow", "green"
+	 * ]) -> ["green", "yellow", "blue", "green", "purple" ] reverseList( ["jingle",
+	 * "bells", "jingle", "bells", "jingle", "all", "the", "way"} ) -> ["way",
+	 * "the", "all", "jingle", "bells", "jingle", "bells", "jingle"]
 	 */
 	public List<String> reverseList(List<String> stringList) {
-		return null;
+		Stack<String> stackList = new Stack<String>();
+		List<String> reverseOrder = new ArrayList<String>();
+		for (String stringListI : stringList) {
+			stackList.push(stringListI);
+		}
+		while (stackList.size() > 0) {
+			reverseOrder.add(stackList.pop());
+		}
+		return reverseOrder;
 	}
 
 	/*
-	 Given an array of ints, divide each int by 2, and return an ArrayList of Doubles.
-	 arrayInt2ListDouble( {5, 8, 11, 200, 97} ) -> [2.5, 4.0, 5.5, 100, 48.5]
-	 arrayInt2ListDouble( {745, 23, 44, 9017, 6} ) -> [372.5, 11.5, 22, 4508.5, 3]
-	 arrayInt2ListDouble( {84, 99, 3285, 13, 877} ) -> [42, 49.5, 1642.5, 6.5, 438.5]
+	 * Given an array of ints, divide each int by 2, and return an ArrayList of
+	 * Doubles. arrayInt2ListDouble( {5, 8, 11, 200, 97} ) -> [2.5, 4.0, 5.5, 100,
+	 * 48.5] arrayInt2ListDouble( {745, 23, 44, 9017, 6} ) -> [372.5, 11.5, 22,
+	 * 4508.5, 3] arrayInt2ListDouble( {84, 99, 3285, 13, 877} ) -> [42, 49.5,
+	 * 1642.5, 6.5, 438.5]
 	 */
 	public List<Double> arrayInt2ListDouble(int[] intArray) {
-		return null;
+		List<Double> doubleList = new ArrayList<Double>();
+		for (int intArrayi : intArray) {
+			Double divide = (intArrayi * 1.0) / 2;
+			doubleList.add(divide);
+		}
+		return doubleList;
 	}
-	
+
 	/*
-	 Given a List of Integers, return the largest value.
-	 findLargest( [11, 200, 43, 84, 9917, 4321, 1, 33333, 8997] ) -> 33333
-	 findLargest( [987, 1234, 9381, 731, 43718, 8932] ) -> 43718
-	 findLargest( [34070, 1380, 81238, 7782, 234, 64362, 627] ) -> 64362
+	 * Given a List of Integers, return the largest value. findLargest( [11, 200,
+	 * 43, 84, 9917, 4321, 1, 33333, 8997] ) -> 33333 findLargest( [987, 1234, 9381,
+	 * 731, 43718, 8932] ) -> 43718 findLargest( [34070, 1380, 81238, 7782, 234,
+	 * 64362, 627] ) -> 64362
 	 */
 	public Integer findLargest(List<Integer> integerList) {
-		return null;
+		Integer largest = 0;
+		for (Integer integerI : integerList) {
+			if (integerI > largest) {
+				largest = integerI;
+			}
+		}
+
+		return largest;
 	}
-	
+
 	/*
-	 Given an array of Integers, return a List of Integers containing just the odd values.
-	 oddOnly( {112, 201, 774, 92, 9, 83, 41872} ) -> [201, 9, 83]
-	 oddOnly( {1143, 555, 7, 1772, 9953, 643} ) -> [1143, 555, 7, 9953, 643]
-	 oddOnly( {734, 233, 782, 811, 3, 9999} ) -> [233, 811, 3, 9999]  
+	 * Given an array of Integers, return a List of Integers containing just the odd
+	 * values. oddOnly( {112, 201, 774, 92, 9, 83, 41872} ) -> [201, 9, 83] oddOnly(
+	 * {1143, 555, 7, 1772, 9953, 643} ) -> [1143, 555, 7, 9953, 643] oddOnly( {734,
+	 * 233, 782, 811, 3, 9999} ) -> [233, 811, 3, 9999]
 	 */
 	public List<Integer> oddOnly(Integer[] integerArray) {
-		return null;
+		List<Integer> onlyOdd = new ArrayList<Integer>();
+		for (Integer integerI : integerArray) {
+			if (integerI % 2 == 1) {
+				onlyOdd.add(integerI);
+			}
+		}
+		return onlyOdd;
 	}
-	
-	/* 
-	 Given a List of Integers, and an int value, return true if the int value appears two or more times in 
-	 the list.
-	 foundIntTwice( [5, 7, 9, 5, 11], 5 ) -> true
-	 foundIntTwice( [6, 8, 10, 11, 13], 8 -> false
-	 foundIntTwice( [9, 23, 44, 2, 88, 44], 44) -> true
+
+	/*
+	 * Given a List of Integers, and an int value, return true if the int value
+	 * appears two or more times in the list. foundIntTwice( [5, 7, 9, 5, 11], 5 )
+	 * -> true foundIntTwice( [6, 8, 10, 11, 13], 8 -> false foundIntTwice( [9, 23,
+	 * 44, 2, 88, 44], 44) -> true
 	 */
 	public boolean foundIntTwice(List<Integer> integerList, int intToFind) {
-		return false;
+		int counter = 0;
+		for (Integer integerI : integerList) {
+			if (integerI == intToFind) {
+				counter++;
+			}
+		}
+		return counter >= 2;
 	}
-	
+
 	/*
-	 Given an array of Integers, return a List that contains the same Integers (as Strings). Except any multiple of 3
-    should be replaced by the String "Fizz", any multiple of 5 should be replaced by the String "Buzz",
-    and any multiple of both 3 and 5 should be replaced by the String "FizzBuzz"
-    ** INTERVIEW QUESTION **	 
-    
-    fizzBuzzList( {1, 2, 3} )  ->  [1, 2, "Fizz"]
-	 fizzBuzzList( {4, 5, 6} )  ->  [4, "Buzz", 6]
-	 fizzBuzzList( {7, 8, 9, 10, 11, 12, 13, 14, 15} )  ->  [7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"]
-	 
-	 HINT: To convert an integer x to a string you can call x.toString() in your code (e.g. if x = 1 then x.ToString() equals "1")
+	 * Given an array of Integers, return a List that contains the same Integers (as
+	 * Strings). Except any multiple of 3 should be replaced by the String "Fizz",
+	 * any multiple of 5 should be replaced by the String "Buzz", and any multiple
+	 * of both 3 and 5 should be replaced by the String "FizzBuzz" INTERVIEW
+	 * QUESTION **
+	 * 
+	 * fizzBuzzList( {1, 2, 3} ) -> [1, 2, "Fizz"] fizzBuzzList( {4, 5, 6} ) -> [4,
+	 * "Buzz", 6] fizzBuzzList( {7, 8, 9, 10, 11, 12, 13, 14, 15} ) -> [7, 8,
+	 * "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"]
+	 * 
+	 * HINT: To convert an integer x to a string you can call x.toString() in your
+	 * code (e.g. if x = 1 then x.ToString() equals "1")
 	 */
 	public List<String> fizzBuzzList(Integer[] integerArray) {
-		return null;
+		String multiple3 = "Fizz";
+		String multiple5 = "Buzz";
+		String multiple3And5 = "FizzBuzz";
+		List<String> newArray = new ArrayList<String>();
+
+		for (Integer integerI : integerArray) {
+			if (integerI % 3 == 0 && integerI % 5 == 0) {
+				newArray.add(multiple3And5);
+			} else if (integerI % 3 == 0) {
+				newArray.add(multiple3);
+			} else if (integerI % 5 == 0) {
+				newArray.add(multiple5);
+			} else {
+				newArray.add(integerI.toString());
+			}
+		}
+		return newArray;
 	}
 
 	/*
-	 Given two lists of Integers, interleave them beginning with the first element in the first list followed
-	 by the first element of the second. Continue interleaving the elements until all elements have been interwoven.
-	 Return the new list. If the lists are of unequal lengths, simply attach the remaining elements of the longer
-	 list to the new list before returning it.
-	 interleaveLists( [1, 2, 3], [4, 5, 6] )  ->  [1, 4, 2, 5, 3, 6]
+	 * Given two lists of Integers, interleave them beginning with the first element
+	 * in the first list followed by the first element of the second. Continue
+	 * interleaving the elements until all elements have been interwoven. Return the
+	 * new list. If the lists are of unequal lengths, simply attach the remaining
+	 * elements of the longer list to the new list before returning it.
+	 * interleaveLists( [1, 2, 3], [4, 5, 6] ) -> [1, 4, 2, 5, 3, 6]
 	 */
 	public List<Integer> interleaveLists(List<Integer> listOne, List<Integer> listTwo) {
-		return null;
+		List<Integer> newArray = new ArrayList<Integer>();
+		Queue<Integer> queueOne = new LinkedList<Integer>();
+		queueOne.addAll(listOne);
+		Queue<Integer> queueTwo = new LinkedList<Integer>();
+		queueTwo.addAll(listTwo);
+
+		int maxLength = 0;
+		if (queueOne.size() >= queueTwo.size()) {
+			maxLength = queueOne.size();
+		} else {
+			maxLength = queueTwo.size();
+		}
+
+		for (int i = 0; i < maxLength; i++) {
+			if (!queueOne.isEmpty()) {
+				newArray.add(queueOne.poll());
+			}
+			if (!queueTwo.isEmpty()) {
+				newArray.add(queueTwo.poll());
+
+			}
+
+		}
+		return newArray;
+
 	}
 
 	/*
-	 Given a list of Integers representing seat numbers, group them into ranges 1-10, 11-20, and 21-30.
-	 (Any seat number less than 1, or greater than 30 is invalid, and can be ignored.) Preserve the order
-	 in which the seat number entered their associated group. Return a list of the grouped Integers 1-10,
-	 11-20, and 21-30. (Hint: Think multiple queues)
-	 boardingGate( [1, 13, 43, 22, 8, 11, 30, 2, 4, 14, 21] ) -> [1, 8, 2, 4, 13, 11, 14, 22, 30, 21]
-	 boardingGate( [29, 19, 9, 21, 11, 1, 0, 25, 15, 5, 31] ) -> [9, 1, 5, 19, 11, 15, 29, 21, 25]
-	 boardingGate( [0, -1, 44, 31, 17, 7, 27, 16, 26, 6] ) -> [7, 6, 17, 16, 27, 26]
+	 * Given a list of Integers representing seat numbers, group them into ranges
+	 * 1-10, 11-20, and 21-30. (Any seat number less than 1, or greater than 30 is
+	 * invalid, and can be ignored.) Preserve the order in which the seat number
+	 * entered their associated group. Return a list of the grouped Integers 1-10,
+	 * 11-20, and 21-30. (Hint: Think multiple queues) boardingGate( [1, 13, 43, 22,
+	 * 8, 11, 30, 2, 4, 14, 21] ) -> [1, 8, 2, 4, 13, 11, 14, 22, 30, 21]
+	 * boardingGate( [29, 19, 9, 21, 11, 1, 0, 25, 15, 5, 31] ) -> [9, 1, 5, 19, 11,
+	 * 15, 29, 21, 25] boardingGate( [0, -1, 44, 31, 17, 7, 27, 16, 26, 6] ) -> [7,
+	 * 6, 17, 16, 27, 26]
 	 */
 	public List<Integer> boardingGate(List<Integer> seatNumberList) {
-		return null;
+		List<Integer> newList = new ArrayList<Integer>();
+		List<Integer> oneToTen = new ArrayList<Integer>();
+		List<Integer> elevenToTwenty = new ArrayList<Integer>();
+		List<Integer> twentyOneToThirty = new ArrayList<Integer>();
+		for (Integer seatNumber : seatNumberList) {
+			if (seatNumber >= 1 && seatNumber <= 10) {
+				oneToTen.add(seatNumber);
+			}
+			if (seatNumber >= 11 && seatNumber <= 20) {
+				elevenToTwenty.add(seatNumber);
+			}
+			if (seatNumber >= 21 && seatNumber <= 30) {
+				twentyOneToThirty.add(seatNumber);
+			}
+		}
+		newList.addAll(oneToTen);
+		newList.addAll(elevenToTwenty);
+		newList.addAll(twentyOneToThirty);
+
+		return newList;
 	}
 
 }
