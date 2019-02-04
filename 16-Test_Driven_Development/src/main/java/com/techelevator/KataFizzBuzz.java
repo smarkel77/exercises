98de038c7bcfab92986e4db23b481838a12b3f92
Steven Markel <smarkel@gmail.com>
2019-02-04 16:16:33 -0500

    asserted - number_15_returns_fizzbuzz

--- 16-Test_Driven_Development/src/main/java/com/techelevator/KataFizzBuzz.java
+++ 16-Test_Driven_Development/src/main/java/com/techelevator/KataFizzBuzz.java
@@ -7,12 +7,17 @@ public class KataFizzBuzz {
 		String fizz = "Fizz";
 		String buzz = "Buzz";
 		output = Integer.toString(input);
+		if (input % 3 == 0 && input % 5 == 0) {
+			return fizz + buzz;
+		}
+		
 		if (input % 3 == 0) {
 			return fizz;
 		}
 		if (input % 5 == 0) {
 			return buzz;
 		}
+		
 		
 		return output;
 	}
