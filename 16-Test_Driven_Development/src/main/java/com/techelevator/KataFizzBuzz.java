e8425457d7dbcc0a649260ed1b56ddbfdbbc23b6
Steven Markel <smarkel@gmail.com>
2019-02-04 16:09:50 -0500

    test number_1_returns_string1a

--- 16-Test_Driven_Development/src/main/java/com/techelevator/KataFizzBuzz.java
+++ 16-Test_Driven_Development/src/main/java/com/techelevator/KataFizzBuzz.java
@@ -3,7 +3,13 @@ package com.techelevator;
 public class KataFizzBuzz {
 	
 	public static String fizzBuzz(int input) {
-		return null;
+		String output = "";
+		String fizz = "Fizz";
+		String buzz = "Buzz";
+		output = Integer.toString(input);
+		
+		
+		return output;
 	}
 
 }
--- 16-Test_Driven_Development/src/test/java/com/techelevator/KataFizzBuzzTest.java
+++ 16-Test_Driven_Development/src/test/java/com/techelevator/KataFizzBuzzTest.java
@@ -5,6 +5,14 @@ import org.junit.Assert;
 
 public class KataFizzBuzzTest {
 	
+	
+	@Test
+	public void number_1_returns_string1() {
+		int input = 1;
+		String output = KataFizzBuzz.fizzBuzz(input);
+		Assert.assertEquals("1", output);
+		
+	}
 	@Test
 	public void number_3_returns_fizz() {
 		int input = 3;
