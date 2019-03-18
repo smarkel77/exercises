142f1a2530bb363cbb8356f62b096698d6d58ec0
Steven Markel <smarkel@gmail.com>
2019-03-18 14:49:07 -0400

    everything except second challenge

--- 46-Introduction_to_JavaScript/challenge-exercises.js
+++ 46-Introduction_to_JavaScript/challenge-exercises.js
@@ -1,1 +1,35 @@
-﻿
+﻿function iqTest(x){
+    let arrOfNums = x.split(" ");
+    let even = 0;
+    let odd = 0;
+    if (x == ""){
+        return 0;
+    }
+    for (let i = 0; i < arrOfNums.length; i++){
+        if (arrOfNums[i] % 2 == 0){
+            even++;
+        } else {
+            odd++;
+        }
+    }
+    if (odd == 0 || even == 0){
+        return 0;
+    }
+    if (even > odd) {
+        for (let i = 0; i < arrOfNums.length; i++){
+            if (arrOfNums[i] % 2 == 1){
+                return i + 1;
+            } 
+        }
+    }
+    if (odd > even) {
+        for (let i = 0; i < arrOfNums.length; i++){
+            if (arrOfNums[i] % 2 == 0){
+                return i + 1;
+            } 
+        }
+    }
+    if (odd == even){
+        return 0;
+    }
+}
--- 46-Introduction_to_JavaScript/exercises.js
+++ 46-Introduction_to_JavaScript/exercises.js
@@ -1,1 +1,100 @@
-﻿
+﻿function sumDouble(x, y){
+    if(x === y) {
+        return (x + y) * 2;
+    }else {
+        return x + y;
+    }
+}
+
+function hasTeen(x, y, z){
+    return ((x >= 13 && x <= 19) || (y >= 13 && y <= 19) || (z >= 13 && z <= 19)); 
+}
+
+function lastDigit(x, y) {
+    return((x % 10) == (y % 10));
+}
+
+function seeColor(color) {
+    if (color.startsWith("red")){
+        return "red";
+    } else if (color.startsWith("blue")){
+        return "blue";
+    } else {
+        return "";
+    }
+}
+
+function oddOnly(arr){
+    let answer = [];
+    for (let i = 0; i < arr.length; i++){
+        if((arr[i] % 2) == 1){
+            answer.push(arr[i]);
+        }
+    }
+    return answer;
+}
+
+function frontAgain(str){
+    front = str.substr(0, 2);
+    end = str.substr(str.length - 2, 2);
+    return (front == end);
+}
+
+function cigarParty(cigar, weekend){
+    if(weekend == true && cigar >= 40){
+        return true;
+    }else {
+        return (cigar >= 40 && cigar <= 60);
+    }
+}
+
+function fizzBuzz(num){
+    if(num % 5 == 0 && num % 3 == 0){
+        return "FizzBuzz";
+    } else if (num % 5 == 0) {
+        return "Buzz";
+    } else if (num % 3 == 0) {
+        return "Fizz";
+    } else {
+        return num;
+    }
+}
+
+function filterEvens(arr){
+    let answer = [];
+    for (let i = 0; i < arr.length; i++){
+        if((arr[i] % 2) == 0){
+            answer.push(arr[i]);
+        }
+    }
+    return answer;
+}
+
+function filterBigNumbers(arr){
+    let answer = [];
+    for (let i = 0; i < arr.length; i++){
+        if(arr[i] >= 100){
+            answer.push(arr[i]);
+        }
+    }
+    return answer;
+}
+
+function filterMultiplesOfX(arr, x){
+    let answer = [];
+    for (let i = 0; i < arr.length; i++){
+        if(arr[i] % x == 0){
+            answer.push(arr[i]);
+        }
+    }
+    return answer;
+}
+
+function createObject(){
+    let person = {
+        firstName : "Steven",
+        lastName : "Markel",
+        age : 41,
+    };
+    return person;
+}
